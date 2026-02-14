db040d2367d24cffc1acae90bce3b9d0f29768d4
Daekeun Kang <dkkang@huconn.com>
2026-02-15 04:02:58 +0900

    chore: Update hangulkeychanger to v2.3.0

--- Casks/hangulkeychanger.rb
+++ Casks/hangulkeychanger.rb
@@ -1,8 +1,8 @@
 cask "hangulkeychanger" do
-  version "2.2.0"
-  sha256 "cc70683ed435c523860581aeb09efd98dc19cab8ee51c95f33b81df3a9aadd0b"
+  version "2.3.0"
+  sha256 "7fe3b849436e2576707bcb0bfb7621361715a8c1e5db18e47b84bab99d2f3948"
 
-  url "https://github.com/hulryung/HangulKeyChanger/releases/download/v#{version}/HangulCommandApp.dmg"
+  url "https://github.com/hulryung/HangulKeyChanger/releases/download/v#{version}/HangulCommandApp-#{version}.dmg"
   name "Hangul Key Changer"
   desc "Remap keys for Korean/English input switching on macOS"
   homepage "https://github.com/hulryung/HangulKeyChanger"
