6fbac7d54407e9393d6ad7b817da0c3c48f769be
Daekeun Kang <dkkang@huconn.com>
2026-03-28 01:35:14 +0900

    chore: Update hangulkeychanger to v2.4.0

--- Casks/hangulkeychanger.rb
+++ Casks/hangulkeychanger.rb
@@ -1,15 +1,15 @@
 cask "hangulkeychanger" do
-  version "2.3.1"
-  sha256 "34e052e88de14d24a1c72e145ae9daa09694c45c4a3d8dd0ac0802e588f97883"
+  version "2.4.0"
+  sha256 "b87b6952be762809bab0f212eea476156088b7a974a79508b560fe0d28c8b7d6"
 
-  url "https://github.com/hulryung/HangulKeyChanger/releases/download/v#{version}/HangulCommandApp-#{version}.dmg"
+  url "https://github.com/hulryung/HangulKeyChanger/releases/download/v#{version}/Hangul.Key.Changer-#{version}.dmg"
   name "Hangul Key Changer"
   desc "Remap keys for Korean/English input switching on macOS"
   homepage "https://github.com/hulryung/HangulKeyChanger"
 
   depends_on macos: ">= :sonoma"
 
-  app "HangulCommandApp.app"
+  app "Hangul Key Changer.app"
 
   zap trash: [
     "~/Library/LaunchAgents/com.hangulcommand.keymap.plist",
