46da4969ba6273a640746ed52aadee81f97c0301
Daekeun Kang <dkkang@huconn.com>
2026-04-14 16:21:58 +0900

    hiterm 0.3.5

--- Casks/hiterm.rb
+++ Casks/hiterm.rb
@@ -1,6 +1,6 @@
 cask "hiterm" do
-  version "0.3.4"
-  sha256 "efa99962fd6ec55ad31a0a117f8420cd19351eba8d02b2e3e0a5d119d841e5a5"
+  version "0.3.5"
+  sha256 "0597353e2bafeb3fbf3d2629ebf2caff4c841584aa0a5ef1278b35e1d5186ff5"
 
   url "https://github.com/hulryung/hiterm/releases/download/v#{version}/hiterm-#{version}.dmg"
   name "hiterm"
