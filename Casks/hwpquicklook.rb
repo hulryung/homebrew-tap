06881f5b5a5487cd21c25e31b7fc2c545e156780
Daekeun Kang <dkkang@huconn.com>
2026-04-28 15:27:31 +0900

    Update hwpquicklook to v0.4.1

--- Casks/hwpquicklook.rb
+++ Casks/hwpquicklook.rb
@@ -1,6 +1,6 @@
 cask "hwpquicklook" do
-  version "0.4.0"
-  sha256 "2a24b94b61a0e85c48f7c3c4fded37f9e914bd2287acc03e2137227d8b7b96d4"
+  version "0.4.1"
+  sha256 "b8d176c6a3850c15dacedfffcb44380a49365a4ce9373d501a6fe81d3020653d"
 
   url "https://github.com/hulryung/hwpql/releases/download/v#{version}/HWPQuickLook-v#{version}.dmg"
   name "HWP Quick Look"
