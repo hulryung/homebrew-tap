8064f105efd232e3ce9c5a01635c2559e0534c33
Daekeun Kang <dkkang@huconn.com>
2026-07-04 19:32:05 +0900

    Fix deprecated macOS requirement form

--- Casks/markdown-prism.rb
+++ Casks/markdown-prism.rb
@@ -7,7 +7,7 @@ cask "markdown-prism" do
   desc "Native macOS Markdown viewer and editor with live preview"
   homepage "https://prism.huconn.xyz"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "MarkdownPrism.app"
 
