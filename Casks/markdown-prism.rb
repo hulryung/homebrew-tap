e5e534fe5942eea3742ec653e9ad3428413ee705
Daekeun Kang <dkkang@huconn.com>
2026-08-03 01:00:20 +0900

    Update markdown-prism to v0.6.1

--- Casks/markdown-prism.rb
+++ Casks/markdown-prism.rb
@@ -1,6 +1,6 @@
 cask "markdown-prism" do
-  version "0.6.0"
-  sha256 "dd4f3fde9c9a2bb85ac81bbe25c41549d305b0e16faa382d36a3bddfc9de4718"
+  version "0.6.1"
+  sha256 "d7d25cba8f01a987b80f9e1d7cd55d1fd392fc632534f0f62599f8ddb9f32527"
 
   url "https://github.com/hulryung/markdown-prism/releases/download/v#{version}/MarkdownPrism-#{version}.dmg"
   name "Markdown Prism"
